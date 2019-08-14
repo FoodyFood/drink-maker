556bf5571d8efe7f5d8633677a97d471acff7063
D O'Connor <d1@outlook.ie>
2019-08-14 20:24:16 +0100

    Update DrinkMachineRev01.ino

--- DrinkMachineRev01.ino
+++ DrinkMachineRev01.ino
@@ -1,5 +1,5 @@
 /*  Arduino code to mix drinks in parallel for cocktails from serial input
-    Copyright (C) 2015  Daniel O'Connor
+    Copyright (C) 2015  D O'Connor
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
